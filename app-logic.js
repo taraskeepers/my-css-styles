1aeb10674f26f7c74e4895a48c6a59ca15793c2c
ppcexperts <ppc.experts.group@gmail.com>
2025-04-30 21:55:51 -0600

    Update app-logic.js

--- app-logic.js
+++ app-logic.js
@@ -768,6 +768,9 @@ span.style.color = "#333";
   setTimeout(() => {
   styleHomeTableHistoryBoxes();
 }, 100);
+  document.querySelectorAll('.history-empty-box, .history-empty-share-box').forEach(el => {
+  void el.offsetHeight;
+});
 
   // 11) Finally, draw the map as before
   const mapData = buildHomeDataForMap();
