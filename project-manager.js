69d6d5bf0e97140d6691807ddddf03d0b4fd6c68
ppcexperts <ppc.experts.group@gmail.com>
2025-07-04 16:33:19 -0600

    Update project-manager.js

--- project-manager.js
+++ project-manager.js
@@ -361,9 +361,6 @@ if (window.dataPrefix !== newPrefix || isActuallyDifferentProject) {
     .then(() => {
       console.log("[POPULATEPROJECTPAGE] Calling populateProjectPage after data reload at:", new Date().toISOString());
       populateProjectPage();
-            if (typeof checkGoogleAdsDataAvailability === 'function') {
-        checkGoogleAdsDataAvailability(project.project_number);
-      }
     })
     .catch(err => {
       console.error("[renderProjects] ❌ switchAccountAndReload error:", err);
@@ -968,9 +965,6 @@ Promise.all([
       productPerformance: productRec.data,
       locationRevenue: locationRec.data
     };
-    if (typeof updateGoogleAdsButtonState === 'function') {
-      updateGoogleAdsButtonState(projectNumber);
-    }
   } else {
     // NO AUTO-FETCH! Just set empty data
     console.log("[Google Sheets] No data for project", projectNumber, "- user hasn't uploaded sheets for this project");
@@ -978,10 +972,6 @@ Promise.all([
       productPerformance: [],
       locationRevenue: []
     };
-        if (typeof updateGoogleAdsButtonState === 'function') {
-      updateGoogleAdsButtonState(projectNumber);
-    }
-  }
   }
 }).catch(err => {
   console.log("[Google Sheets] Using empty data due to error:", err.message);
@@ -989,9 +979,6 @@ Promise.all([
     productPerformance: [],
     locationRevenue: []
   };
-    if (typeof updateGoogleAdsButtonState === 'function') {
-    updateGoogleAdsButtonState(projectNumber);
-  }
 });
 
       const loadedProjectNumbers = new Set(window.companyStatsData.map(r => r.project_number));
